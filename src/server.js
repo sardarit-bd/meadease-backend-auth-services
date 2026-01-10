a35e9d899584dff40e7eea343642776a7e96c8b3
Md Emon Hossen <mdemong87@gmail.com>
2026-01-10 11:41:19 +0600

    update all setup

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import environment from "./config/env.js";
+import connectDB from './config/db.js';
+import { connectRedis } from "./config/redis.config.js";
+import app from "./app.js";
+
+/******** PORT Define *******/
+const PORT = process.env.AUTH_SERVICE_PORT || 5001;
+
+
+/********** Connect to Database Here **********/
+await connectDB();
+await connectRedis();
+
+
+/*********** Start The Server ***********/
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on port: ${PORT} in ${environment} mode`);
+  });
+
